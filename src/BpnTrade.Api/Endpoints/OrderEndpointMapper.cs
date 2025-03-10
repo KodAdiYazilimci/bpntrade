ec6aea5a9355e2a5e88a1741107c0f56b11df63d
ayyildiz\desktop <ayyildiz\desktop>
2025-03-10 19:13:52 +0300

    Create order endpoint has been generated

---
+++ src/BpnTrade.Api/Endpoints/OrderEndpointMapper.cs
@@ -0,0 +1,29 @@
+﻿using BpnTrade.Domain.Dto.Order;
+using BpnTrade.Domain.Services;
+
+namespace BpnTrade.Api.Endpoints
+{
+    public static class OrderEndpointMapper
+    {
+        public static RouteGroupBuilder MapOrderEndpoints(this WebApplication app)
+        {
+            var group = app.MapGroup("/order");
+
+            return group;
+        }
+
+        public static RouteGroupBuilder MapCreateOrderEndpoint(this RouteGroupBuilder builder)
+        {
+            builder.MapPut("/", async (CreateOrderRequestDto dto, IOrderService orderService) =>
+            {
+                CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+                var result = await orderService.CreateAsync(dto, cancellationTokenSource.Token);
+
+                return result.IsSuccess ? Results.Ok(result.Data) : Results.BadRequest(result.Error);
+            });
+
+            return builder;
+        }
+    }
+}
